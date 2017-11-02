3bee5f327d9bb2a4c3a2a1a8033b6e96d45d6c7f
Ashley Yang <ay322@cornell.edu>
2017-11-01 22:44:26 -0400

    new site

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 var showText = function (message, index, interval) {
   if (index < message.length) {
     var div = "<div style='font-size:900%; color:white; font-weight:lighter'>"+message[index++]+"</div>"
@@ -16,4 +17,32 @@ var addArrow = function() {
 
 $(function () {
   showText(["Stop \n", "Wasting\n", "Time"], 0, 500);
-});
+}); */
+
+/*
+
+    $(document).ready(function () {
+      setImageOne();
+    });
+
+    function setImageOne() {
+      $('#start').fadeIn(500).html('<img class="first" style="height:100%; width: 100%;" src="images/Slide1.jpg" />').delay(2000).fadeOut(700, function () { setImageTwo(); });
+    }
+
+    function setImageTwo() {
+      $('#start').fadeIn(500).html('<img class="first" style="height:100%; width: 100%;" src="images/Slide2.jpg" />').delay(2000).fadeOut(700, function () { setImageThree(); });
+    }
+
+    function setImageThree(){
+      $('#start').fadeIn(500).html('<img class="first" style="height:100%; width: 100%;" src="images/Slide3.jpg" />').delay(2000).fadeOut(700, function () { setImageFour(); });
+    }
+
+
+    function setImageFour(){
+      $('#start').fadeIn(500).html('<img class="first" style="height:100%; width: 100%;" src="images/Slide4.jpg" />').delay(2000).fadeOut(700, function () { setImageFive(); });
+    }
+
+
+    function setImageFive(){
+      $('#start').fadeIn(500).html('<img class="first" style="height:100%; width: 100%;" src="images/Slide5.jpg" />').delay(2000);
+    } */
